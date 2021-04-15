cfd4b1f394726838ecaa5d369fb0abd6dd4cb754
Javier Beneito Barquero <malsolo.com@outlook.com>
2021-04-15 16:23:45 +0200

    kafka-streams-spring-cloud-stream: project from start.spring.io

---
+++ src/main/java/com/malsolo/kafka/streams/spring/cloud/stream/KafkaStreamsSpringCloudStreamApplication.java
@@ -0,0 +1,13 @@
+package com.malsolo.kafka.streams.spring.cloud.stream;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaStreamsSpringCloudStreamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaStreamsSpringCloudStreamApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/malsolo/kafka/streams/spring/cloud/stream/KafkaStreamsSpringCloudStreamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.malsolo.kafka.streams.spring.cloud.stream;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaStreamsSpringCloudStreamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
